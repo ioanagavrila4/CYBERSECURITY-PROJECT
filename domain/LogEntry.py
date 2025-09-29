95fc536b9020922e0318b65a666c741d8cfcb13c
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-09-29 11:46:11 +0300

    add LogEntry json parsing

--- domain/LogEntry.py
+++ domain/LogEntry.py
@@ -1,5 +1,9 @@
 import json
 
+UNKNOWN = 0
+JSON = 1
+
+
 class LogEntry:
     def __init__(self, raw_format: str):
         self._raw_format = raw_format
@@ -7,15 +11,24 @@ class LogEntry:
         self._severity = None
         self._description = None
         self._hostname = None
+        self._type = JSON
 
-
+        if self._type==JSON:
+            self.parse_json_log()
 
     def parse_json_log(self):
-        pass
+        try:
+            log = json.loads(self._raw_format)
+            self._timestamp = log.get("__REALTIME_TIMESTAMP")
+            self._severity = log.get("PRIORITY")
+            self._description = log.get("MESSAGE")
+            self._hostname = log.get("_HOSTNAME")
+        except:
+            print("Failed to parse log entry")
 
 
 def main():
-    pass
+    test_log = LogEntry('')
 
 if __name__ == "__main__":
     main()
