75ce7ae25b18da2cbafa16022d30bfa83392f784
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-09-29 11:38:47 +0300

    add LogEntry representation

--- domain/LogEntry.py
+++ domain/LogEntry.py
@@ -1,6 +1,18 @@
+import json
+
 class LogEntry:
-    def __init__(self):
+    def __init__(self, raw_format: str):
+        self._raw_format = raw_format
+        self._timestamp = None
+        self._severity = None
+        self._description = None
+        self._hostname = None
+
+
+
+    def parse_json_log(self):
         pass
+
 
 def main():
     pass
