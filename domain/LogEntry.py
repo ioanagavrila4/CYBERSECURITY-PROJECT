bb61a7582f9bdf39a5c279fbd4fdeae0d860f7bb
maiaiia <176808387+maiaiia@users.noreply.github.com>
2025-09-29 12:26:15 +0300

    add Collector class

--- domain/LogEntry.py
+++ domain/LogEntry.py
@@ -3,7 +3,6 @@ import json
 UNKNOWN = 0
 JSON = 1
 
-
 class LogEntry:
     def __init__(self, raw_format: str, log_type=JSON):
         self._raw_format = raw_format
@@ -36,51 +35,13 @@ class LogEntry:
             self._severity = log.get("PRIORITY")
             self._description = log.get("MESSAGE")
             self._hostname = log.get("_HOSTNAME")
-        except json.JSONDecodeError as e:
-            print(f"Failed to parse log entry as JSON: {e}")
-            print(f"Raw format: {self._raw_format[:100]}...")
-        except Exception as e:
-            print(f"Unexpected error parsing log entry: {e}")
+        except:
+            print("Failed to parse log entry")
 
 
 
 def main():
-    test_str = """
-    {
-        "_SYSTEMD_SLICE": "-.slice",
-        "MESSAGE": "run-credentials-systemd\\x2dtmpfiles\\x2dclean.service.mount: Deactivated suc",
-        "_EXE": "/usr/lib/systemd/systemd",
-        "_HOSTNAME": "pibytes3",
-        "__REALTIME_TIMESTAMP": "1759134362452973",
-        "_COMM": "systemd",
-        "_CAP_EFFECTIVE": "1ffffffffff",
-        "_BOOT_ID": "eb295f91d0804552bc6e3441cc1e2bb6",
-        "SYSLOG_IDENTIFIER": "systemd",
-        "PRIORITY": "6",
-        "CODE_FILE": "src/core/unit.c",
-        "_RUNTIME_SCOPE": "system",
-        "_TRANSPORT": "journal",
-        "CODE_FUNC": "unit_log_success",
-        "_SYSTEMD_UNIT": "init.scope",
-        "_UID": "0",
-        "MESSAGE_ID": "7ad2d189f7e94e70a38c781354912448",
-        "_MACHINE_ID": "b29f86b72d8042c4bbdcc14aa31d355f",
-        "CODE_LINE": "5618",
-        "__MONOTONIC_TIMESTAMP": "928790179",
-        "_SYSTEMD_CGROUP": "/init.scope",
-        "__CURSOR": "s=087efa337c6648a2abe0eed6a087d677;i=4a3;b=eb295f91d0804552bc6e3441cc1e2bb6;",
-        "UNIT": "run-credentials-systemd\\x2dtmpfiles\\x2dclean.service.mount",
-        "_SOURCE_REALTIME_TIMESTAMP": "1759134362452958",
-        "TID": "1",
-        "_PID": "1",
-        "_CMDLINE": "/sbin/init splash",
-        "SYSLOG_FACILITY": "3",
-        "_GID": "0",
-        "INVOCATION_ID": "95055cf5adbd4cb3bb661d3468b13cf0"
-    }
-    """
-    test_log = LogEntry(test_str)
-    print(test_log.get_severity())
+    pass
 
 
 if __name__ == "__main__":
